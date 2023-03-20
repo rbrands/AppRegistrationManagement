edb9ebacf4975c6be4c1b5494ae065ada71f031b
Robert Brands <mail@robert-brands.com>
2023-03-20 17:05:21 +0100

    first commit

---
+++ GraphHelper.cs
@@ -0,0 +1,86 @@
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Me;
+
+class GraphHelper
+{
+    // Settings object
+    private static Settings? _settings;
+    // User auth token credential
+    private static DeviceCodeCredential? _deviceCodeCredential;
+    // Client configured with user authentication
+    private static GraphServiceClient? _userClient;
+
+    public static void InitializeGraphForUserAuth(Settings settings,
+        Func<DeviceCodeInfo, CancellationToken, Task> deviceCodePrompt)
+    {
+        _settings = settings;
+
+        _deviceCodeCredential = new DeviceCodeCredential(deviceCodePrompt,
+            settings.TenantId, settings.ClientId);
+
+        _userClient = new GraphServiceClient(_deviceCodeCredential, settings.GraphUserScopes);
+    }
+    public static async Task<string> GetUserTokenAsync()
+    {
+        // Ensure credential isn't null
+        _ = _deviceCodeCredential ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        // Ensure scopes isn't null
+        _ = _settings?.GraphUserScopes ?? throw new System.ArgumentNullException("Argument 'scopes' cannot be null");
+
+        // Request token with given scopes
+        var context = new TokenRequestContext(_settings.GraphUserScopes);
+        var response = await _deviceCodeCredential.GetTokenAsync(context);
+        return response.Token;
+    }
+
+    public static Task<User?> GetUserAsync()
+    {
+        // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        return _userClient.Me
+            .GetAsync((requestConfiguration) => 
+            {
+                // Only request specific properties
+                requestConfiguration.QueryParameters.Select = new [] { "displayName", "mail", "userPrincipalName" };
+            });
+    }
+    // This function serves as a playground for testing Graph snippets
+    // or other code
+    public async static Task<ApplicationCollectionResponse?> ListApplicationsAsync()
+    {
+         // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+        var applications = await _userClient.Applications.GetAsync();
+        return applications;
+   }
+    public async static Task<ServicePrincipalCollectionResponse?> ListServicePrincipalsAsync()
+    {
+         // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+        var servicePrincipals = await _userClient.ServicePrincipals.GetAsync((config) => 
+        {
+            config.QueryParameters.Top = 900;
+        });
+        return servicePrincipals;
+   }
+    public async static Task<ServicePrincipalCollectionResponse?> ListServicePrincipalsWithoutMicrosoftAppsAsync()
+    {
+         // Ensure client isn't null
+        _ = _userClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+        var servicePrincipals = await _userClient.ServicePrincipals.GetAsync((config) => 
+        {
+            config.QueryParameters.Top = 900;
+        });
+        return servicePrincipals;
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,147 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Created via tutorial https://learn.microsoft.com/en-us/graph/tutorials/dotnet?tabs=aad
+Console.WriteLine(".NET Graph AppRegistrations\n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+InitializeGraph(settings);
+
+// Greet the user by name
+await GreetUserAsync();
+
+int choice = -1;
+
+while (choice != 0)
+{
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Display access token");
+    Console.WriteLine("2. List ServicePrincipals excluding Microsoft apps");
+    Console.WriteLine("3. List ServicePrincipals");
+    Console.WriteLine("4. List applications");
+
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException)
+    {
+        // Set to invalid value
+        choice = -1;
+    }
+
+    switch(choice)
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodbye...");
+            break;
+        case 1:
+            // Display access token
+            await DisplayAccessTokenAsync();
+            break;
+        case 2:
+            await ListServicePrincipalsAsync(true);
+            break;
+        case 3:
+            // List ServicePrincipals
+            await ListServicePrincipalsAsync();
+            break;
+        case 4:
+            // List applications
+            await ListApplicationsAsync();
+            break;
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+}
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForUserAuth(settings,
+        (info, cancel) =>
+        {
+            // Display the device code message to
+            // the user. This tells them
+            // where to go to sign in and provides the
+            // code to use.
+            Console.WriteLine(info.Message);
+            return Task.FromResult(0);
+        });
+}
+
+async Task GreetUserAsync()
+{
+    try
+    {
+        var user = await GraphHelper.GetUserAsync();
+        Console.WriteLine($"Hello, {user?.DisplayName}!");
+        // For Work/school accounts, email is in Mail property
+        // Personal accounts, email is in UserPrincipalName
+        Console.WriteLine($"Email: {user?.Mail ?? user?.UserPrincipalName ?? ""}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user: {ex.Message}");
+    }
+}
+
+async Task DisplayAccessTokenAsync()
+{
+    try
+    {
+        var userToken = await GraphHelper.GetUserTokenAsync();
+        Console.WriteLine($"User token: {userToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user access token: {ex.Message}");
+    }
+}
+
+
+async Task ListApplicationsAsync()
+{
+    try
+    {
+        var applications = await GraphHelper.ListApplicationsAsync();
+        Console.WriteLine($"# Applications: {applications?.Value?.Count()}");
+        if (null != applications?.Value)
+        {
+            foreach (var app in applications.Value)
+            {
+                Console.WriteLine($"{app.DisplayName}");
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting applications: {ex.Message}");
+    }
+
+}
+
+async Task ListServicePrincipalsAsync(bool withoutMsApps = false)
+{
+    try
+    {
+        var servicePrincipals = withoutMsApps? await GraphHelper.ListServicePrincipalsWithoutMicrosoftAppsAsync() : await GraphHelper.ListServicePrincipalsAsync();
+        Console.WriteLine($"# ServicePrincipals: {servicePrincipals?.Value?.Count()}");
+        if (null != servicePrincipals?.Value)
+        {
+            foreach (var spn in servicePrincipals.Value)
+            {
+                if ( !withoutMsApps || spn.AppOwnerOrganizationId.ToString() != settings.MicrosoftAppTenantId)
+                {
+                    Console.WriteLine($"{spn.DisplayName} - {spn.AppId} - App Owner Tenant {spn.AppOwnerOrganizationId} - {spn.ServicePrincipalType}" );
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting applications: {ex.Message}");
+    }
+
+}
